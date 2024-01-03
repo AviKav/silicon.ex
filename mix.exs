d6e2427b2212660843448d022f89a7102ff3f14a
noteven <15650992+noteven@users.noreply.github.com>
2024-01-03 17:40:55 +0500

    Add package and doc declaration to mix.exs for publishing.

--- mix.exs
+++ mix.exs
@@ -1,10 +1,22 @@
 defmodule Silicon.MixProject do
   use Mix.Project
 
+
+  @description """
+  """
+  @version "./VERSION" |> File.read!() |> String.trim()
+  @repo_url "https://github.com/AviKav/silicon.ex"
+  @app_name :silicon
+
   def project do
     [
-      app: :silicon,
-      version: "0.1.0",
+      app: @app_name,
+      description: @description,
+      version: @version,
+      source_url: @repo_url,
+      homepage_url: @repo_url,
+      docs: docs(),
+      package: package(),
       elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps()
@@ -15,6 +27,38 @@ defmodule Silicon.MixProject do
   def application do
     [
       extra_applications: [:logger]
+    ]
+  end
+
+  def docs do
+    [
+      name: to_string(@app_name),
+      source_ref: "v#{@version}",
+      extras: ~w[README.md LICENSE VERSION],
+      main: "README.md",
+      source_url: @repo_url,
+    ]
+  end
+
+  def package do
+    [
+      name: @app_name,
+      description: @description,
+      maintainers: [],
+      licenses: ["MPL-2.0"],
+      files: ~w(
+        lib
+        native
+        .formatter.exs
+        mix.exs
+        README*
+        LICENSE*
+        CHANGELOG*
+        VERSION
+      ),
+      links: %{
+        GitHub: @repo_url,
+      },
     ]
   end
 
