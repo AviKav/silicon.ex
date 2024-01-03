a015c873acb2174f154c68755319761c863309ad
AviKav <avikav@avikav.net>
2024-01-03 06:57:14 -0500

    Correct missing license header in native.ex

--- lib/native.ex
+++ lib/native.ex
@@ -1,3 +1,7 @@
+#  This Source Code Form is subject to the terms of the Mozilla Public
+#  License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 defmodule Elixir.Silicon.Native do
   use Rustler, otp_app: :silicon, crate: "silicon_nif"
   use TypeCheck
