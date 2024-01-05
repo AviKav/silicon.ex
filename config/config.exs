d9bc5112e321297133e2e50f1610efc5ce1e75cb
noteven <15650992+noteven@users.noreply.github.com>
2024-01-05 17:44:27 +0500

    Initial solution for using precompiled NIF.

---
+++ config/config.exs
@@ -0,0 +1,16 @@
+import Config
+
+case System.get_env("NIF_LOAD_PATH") do
+  nif_path when not is_nil(nif_path) and is_bitstring(nif_path) ->
+    config :silicon, Silicon.Native,
+      load_from: {:silicon, nif_path},
+      skip_compilation?: true
+
+  nil ->
+    config :silicon, Silicon.Native,
+      skip_compilation?: false
+end
+
+
+env_conf = "#{config_env()}.exs"
+if File.exists?(env_conf), do: import_config(env_conf)
