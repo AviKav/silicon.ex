dd38699ab9b8e0d3eef2207b1009073772c28ccc
noteven <15650992+noteven@users.noreply.github.com>
2024-01-06 16:52:56 +0500

    Use if instead of case statement.

--- config/config.exs
+++ config/config.exs
@@ -1,14 +1,10 @@
 import Config
 
-case System.get_env("NIF_LOAD_PATH") do
-  nif_path when not is_nil(nif_path) and is_bitstring(nif_path) ->
-    config :silicon, Silicon.Native,
-      load_from: {:silicon, nif_path},
-      skip_compilation?: true
-
-  nil -> true # Keep default Rustler behaviour
+if nif_path = System.get_env("NIF_LOAD_PATH") do
+   config :silicon, Silicon.Native,
+     load_from: {:silicon, nif_path},
+     skip_compilation?: true
 end
-
 
 env_conf = "#{config_env()}.exs"
 if File.exists?(env_conf), do: import_config(env_conf)
